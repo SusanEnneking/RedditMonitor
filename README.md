d002c73f80e878fc51b40c0f8faa88763b66deda
Susan Enneking <susan.enneking@gmail.com>
2024-01-14 08:07:35 -0600

    monitor a subreddit needs work

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Reddit Monitor #
+This project is a little .net Core exercise that monitors a subreddit topic and reports on:
+- Posts with most up votes
+- Users with most posts
+
 ## Information to help get the initial tokens needed ##
 [Help me get an app](https://www.reddit.com/prefs/apps)\
 [Help me get tokens](https://docs.aws.amazon.com/solutions/latest/discovering-hot-topics-using-machine-learning/retrieve-and-manage-api-credentials-for-reddit-api-authentication.html)
--- RedditMonitor/Services/RedditMonitorService.cs
+++ RedditMonitor/Services/RedditMonitorService.cs
@@ -1,17 +1,16 @@
-using System;
 using Reddit;
+using Reddit.Controllers;
+using Reddit.Controllers.EventArgs;
+
 namespace RedditMonitor.Services;
-
-
 public class RedditMonitor
 {
-    string? _appId;
-    string? _appSecret;
-    string? _refreshToken;
+    readonly string? _appId;
+    readonly string? _appSecret;
+    readonly string? _refreshToken;
     DateTime _monitorStarttime;
 
     public RedditMonitor(){
-        _monitorStarttime = DateTime.Now;
         DotNetEnv.Env.Load("../.env");
         _appId = Environment.GetEnvironmentVariable("REDDIT_APPID");
         _appSecret = Environment.GetEnvironmentVariable("REDDIT_SECRET");
@@ -19,12 +18,21 @@ public class RedditMonitor
         var redditClient = new RedditClient(appId: _appId, 
                     appSecret: _appSecret, 
                     refreshToken: _refreshToken);
-        // Get info on another subreddit.
-        var askReddit = redditClient.Subreddit("AskReddit").About();
-
-        // Get the top post from a subreddit.
-        var topPost = askReddit.Posts.Top[0];
+        Subreddit sub = redditClient.Subreddit("AskReddit").About();
+        sub.Posts.NewUpdated += C_NewPostsUpdated;
+        _monitorStarttime = DateTime.Now;
+        sub.Posts.MonitorNew();
     }
+
     public DateTime MonitorStartTime => _monitorStarttime;
+    // Using monitor example from Reddit.DOTNET examples
+    // https://github.com/sirkris/Reddit.NET/blob/master/src/Example/Program.cs
+    public static void C_NewPostsUpdated(object sender, PostsUpdateEventArgs e)
+    {
+        foreach (Post post in e.Added)
+        {
+            Console.WriteLine("[" + post.Subreddit + "] New Post by " + post.Author + ": " + post.Title);
+        }
+    }
 
 }
